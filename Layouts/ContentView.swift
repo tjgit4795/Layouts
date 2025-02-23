bfcf9ef3c86b06c183fbf9e13c752a24a7ac9c1b
Tim Ruhl <timjruhl@gmail.com>
2025-02-23 13:09:06 -0500

    Initial Commit

---
+++ Layouts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layouts
+//
+//  Created by Michelle Ruhl on 2/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Michelle Ruhl on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
